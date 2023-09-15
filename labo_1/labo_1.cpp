fc49f93b2a5f36882752b01d71ccce7b2aedf5ef
Locor99 <louiscormier1@hotmail.com>
2023-09-14 20:08:58 -0400

    hello world program

--- labo_1/labo_1.cpp
+++ labo_1/labo_1.cpp
@@ -1,3 +1,11 @@
-//
-// Created by louis on 2023-09-14.
-//
+#include <cstdio>
+
+int main() {
+    printf("Hello, World!\n");
+
+    // Add a line to wait for user input
+    printf("Press Enter to exit...");
+    getchar(); // Wait for a key press
+
+    return 0;
+}
