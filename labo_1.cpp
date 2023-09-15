d5eade3f764d601233d33e276a57135a69a7a8b6
Locor99 <louiscormier1@hotmail.com>
2023-09-15 09:44:27 -0400

    cLion Build

--- labo_1.cpp
+++ labo_1.cpp
@@ -1,11 +1,32 @@
-#include <cstdio>
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+std::unordered_map<char, int> CountCharacters(const std::string& input) {
+    std::unordered_map<char, int> charCount;
+
+    for (char c : input) {
+        charCount[c]++;
+    }
+
+    return charCount;
+}
+
+void PrintCharacterCounts(const std::unordered_map<char, int>& charCount) {
+    for (const auto& pair : charCount) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    while (true) {
+        std::string input;
+        std::cout << "Enter a string: ";
+        std::cin >> input;
 
-    // Add a line to wait for user input
-    printf("Press Enter to exit...");
-    getchar(); // Wait for a key press
+        std::unordered_map<char, int> charCount = CountCharacters(input);
+        PrintCharacterCounts(charCount);
+    }
 
     return 0;
 }
